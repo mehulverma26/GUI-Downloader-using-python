77b01f2864c7973b2c599971c7662be6ffe1abe5
Mehul Verma <mehulverma04@gmail.com>
2023-06-08 07:30:27 +0530

    base design

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, Tk
 import requests
 
-class Downloaader:
+class Downloader:
     def __init__(self):
         self.window=tk.Tk()
         self.window.title("GUI Downloader")
@@ -11,13 +11,25 @@ class Downloaader:
         self.url_entry=tk.Entry()
         self.url_entry.pack()
         self.browse_button=tk.Button(text="Browser", command=self.browse_file)
+        self.browse_button.pack()
+        self.download_button=tk.Button(text="Download", command=self.download)
+        self.download_button.pack()
+        self.window.mainloop()
+    
+    def browse_file(self):
+        pass
 
-def downloadFile():
-    pass
+    def download(self):
+        pass
 
-root=Tk()
-frm=ttk.Frame(root,padding=10)
-frm.grid()
-ttk.Label(frm,text="display").grid(column=1,row=0)
-ttk.Button(frm,text="exit",command=root.destroy).grid(column=2,row=0)
-root.mainloop()
+Downloader()
+
+# def downloadFile():
+#     pass
+
+# root=Tk()
+# frm=ttk.Frame(root,padding=10)
+# frm.grid()
+# ttk.Label(frm,text="display").grid(column=1,row=0)
+# ttk.Button(frm,text="exit",command=root.destroy).grid(column=2,row=0)
+# root.mainloop()
