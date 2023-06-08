e40852e6c50ff7da46eb045ddae52d91d6ac01a7
Mehul Verma <mehulverma04@gmail.com>
2023-06-08 06:58:19 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import ttk
+root=ttk()
+frm=ttk.Frame(root,padding=10)
+frm.grid()
+ttk.Label(frm,text="display").grid(column=1,row=0)
+ttk.Button(frm,text="exit",command=root.destroy).grid(column=2,row=0)
+root.mainloop()
